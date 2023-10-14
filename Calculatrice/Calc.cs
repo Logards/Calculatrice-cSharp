6afeb3ddd18e272c99730224c8f5aee38901241d
Logards <riccibastien@gmail.com>
2023-10-14 10:44:40 +0200

    Update

--- Calculatrice/Calc.cs
+++ Calculatrice/Calc.cs
@@ -10,17 +10,37 @@ public class Calc
 
     public static string Operator(string str)
     {
-        MatchCollection matches = Regex.Matches(str, @"\d+");
+        MatchCollection matches = Regex.Matches(str, @"-?\d+(?:\,\d+)?");
 
         double[] result = matches
             .Select(match => Convert.ToDouble(match.Value))
             .ToArray();
-        if (result.Length > 2)
+        if (result.Length > 2 || result.Length >= 2 && str.Contains("^²") || result.Length >= 2 && str.Contains("\u221a"))
         {
             return "Only one Operation";
         } else if (str.Length == 0 || result.Length == 0)
         {
             return "Please enter an operation";
+        } else if (result.Length == 1)
+        {
+            if (str.Contains("^²"))
+            {
+                return (result[0] * result[0]).ToString();
+            } else if (str.Contains("\u221a"))
+            {
+                if (str[str.Length - 2].ToString() == "\u221a")
+                {
+                    return "Invalid format";
+                }
+                else
+                {
+                    return Math.Sqrt(result[0]).ToString();
+                }
+            }
+            else
+            {
+                return result[0].ToString();
+            }
         }
 
         if (str.Contains("+"))
@@ -49,6 +69,16 @@ public class Calc
                 return (result[0] / result[1]).ToString();
             }
         }
-        return (result[0] % result[1]).ToString();
+
+        if (str.Contains("%"))
+        {
+            return (result[0] % result[1]).ToString();
+        }
+
+        if (str.Contains("+/-"))
+        {
+        }
+        
+        return result[0].ToString();
     }
 } 
--- Calculatrice/CalculatorScreen.axaml.cs
+++ Calculatrice/CalculatorScreen.axaml.cs
@@ -11,18 +11,42 @@ public partial class CalculatriceScreen : Window
     {
         InitializeComponent();
     }
+
+    void Delete(object? sender, RoutedEventArgs args)
+    {
+        if (Operation.Content.ToString().Length > 0)
+        {
+            Operation.Content = (Operation.Content.ToString()).Remove(Operation.Content.ToString().Length - 1);
+        }
+    }
+
+    void Clear(object? sender, RoutedEventArgs args)
+    {
+        Operation.Content = null;
+        Result.Content = null;
+    }
+    void ClearE(object? sender, RoutedEventArgs args)
+    {
+        Operation.Content = null;
+    }
     
     void Add(object? sender, RoutedEventArgs args)
     {
         string calcul;
-        string result;
         Button b =  (sender as Button)!;
+        if (b.Content.ToString() == "1/x")
+        {
+            Operation.Content += "1/";
+        }
+        else
+        {
+            Operation.Content += b.Content.ToString();
+        }
         if (b.Content.ToString() == "=")
         {
             calcul = Operation.Content.ToString();
             Result.Content = Calc.Operator(calcul);
             Operation.Content = "";
         }
-        Operation.Content += b.Content.ToString();
     }
 }
